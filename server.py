21d2bfcd78231bf7c115173e3ea946ae8bf5110e
jordanforbes <jordforb@gmail.com>
2020-05-08 17:17:49 -0400

    created static files

--- server.py
+++ server.py
@@ -3,11 +3,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def start():
-    return render_template('index.html',times = 8)
+    return render_template('index.html',times = 8, halfTimes=4)
 
 @app.route('/<num>')
 def buildboard(num):
-    return render_template('index.html',times= int(num),color1='red',color2='black')
+    return render_template('index.html',times= int(num), halfTimes=4)
+
+@app.route('/<x>/<y>')
+def bothSides(x,y):
+    return render_template('index.html',times =int(x), halfTimes=int(int(y)/2))
 
 if __name__ =="__main__":
     app.run(debug=True)
